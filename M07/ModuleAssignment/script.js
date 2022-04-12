e2d9771895c51f803fdc0c277901b6afaaf12711
ChuProgram <97070822+ChuProgram@users.noreply.github.com>
2022-04-12 13:33:31 -0700

    Update script.js

--- M07/ModuleAssignment/script.js
+++ M07/ModuleAssignment/script.js
@@ -1,3 +1,3 @@
 function hotel() {
-  window.open("https://prog109chu.github.io/mod7/mod7website.html", "popup");
+  window.open("https://chuprogram.github.io/prog109/M07/ModuleAssignment/mod7website.html", "popup");
 }
